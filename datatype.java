59e6d86275217ab40e7b664d09bc990a8e033518
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2025-12-28 16:43:15 +0530

    datatypes example in primitive

---
+++ datatype.java
@@ -0,0 +1,78 @@
+public class datatype {
+    public static void main(String[] args) {
+        int age=19;
+        System.out.println(age);
+
+        
+    }
+    
+ }
+// public class datatype {
+//     public static void main(String[] args) {
+//         byte age=19;
+//         System.out.println(age);
+
+        
+//     }
+    
+// }
+// public class datatype {
+//     public static void main(String[] args) {
+//         short salary=30000;
+//         System.out.println(salary);
+
+        
+//     }
+    
+// }
+// public class datatype {
+//     public static void main(String[] args) {
+//         long number=200000000000l;
+//         System.out.println(number);
+
+        
+//     }
+    
+// }
+// public class datatype {
+//     public static void main(String[] args) {
+//         float mark=95.55f;
+//         System.out.println(mark);
+
+        
+//     }
+    
+// }
+// public class datatype {
+//     public static void main(String[] args) {
+//          double mark=95.555;
+//         System.out.println(mark);
+
+        
+//     }
+    
+// }
+// 
+// public class datatype {
+//     public static void main(String[] args) {
+//         boolean presence =true;
+//         System.out.println(presence);
+
+        
+//     }
+    
+// }
+// public class datatype {
+//     public static void main(String[] args) {
+//         char grade='A';
+//         System.out.println(grade);
+
+        
+//     }
+    
+// }
+
+
+
+
+
