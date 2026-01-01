6c501cc5721b0b98dad7b941cfa3a052f0e2f81d
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-01-01 17:51:45 +0530

    relatinal operator in java

---
+++ relational.java
@@ -0,0 +1,25 @@
+public class relational {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        System.out.println(a<b);
+        int c=7;
+        int d=3;
+        System.out.println(c>d);
+        int e=3;
+        int f=4;
+        System.out.println(e<=f);
+        int g=20;
+        int h=30;
+        System.out.println(g>=h);
+        int i=50;
+        int j=40;
+        System.out.println(i==j);
+        int k=30;
+        int l=20;
+        System.out.println(k!=l);
+
+
+    }
+    
+}
