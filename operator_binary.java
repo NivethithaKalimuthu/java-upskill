2cebf7c2729871f433b88992dbb328a0c8ec231d
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-01-01 17:49:50 +0530

    arithmetic operator in java

---
+++ operator_binary.java
@@ -0,0 +1,26 @@
+public class operator_binary {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+
+        int c=a+b;
+        int d=a-b;
+        int e=a*b;
+        float f=(float)a/(float)b;
+        int g=a%b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+
+        //precedence
+        int x=3;
+        int y=40;
+        int z=9;
+        int zz=(z*y+x);
+        System.out.println(zz);
+        
+    }
+    
+}
