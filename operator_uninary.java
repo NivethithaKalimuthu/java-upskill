54513cd6e1ec575461f90a1d4233776ca95e4964
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-01-01 17:50:51 +0530

    uninary

---
+++ operator_uninary.java
@@ -0,0 +1,13 @@
+public class operator_uninary {
+    public static void main(String[] args) {
+        
+    
+    int a=5;
+    System.out.println(a++);
+    System.out.println(a);
+    int b=9;
+    ++b;
+    System.out.println(b);
+    }
+    
+}
