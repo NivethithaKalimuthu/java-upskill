944c7467baf34efd21636737fac400d23ce21f57
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-01-02 21:14:12 +0530

    nested concept

---
+++ nested.java
@@ -0,0 +1,25 @@
+public class nested {
+    public static void main(String[] args) {
+        char attendence='p';
+        int total=40;
+        if(attendence=='p')
+        {
+            if(total>=50){
+                System.out.println("attended class");
+            }
+            
+            
+        }
+        else if(attendence=='o')
+        {
+            System.out.println("oneduty");
+
+        }
+        else{
+            System.out.println("not attended class");
+        }
+        System.out.println("program ends");
+    }
+    
+}
+
