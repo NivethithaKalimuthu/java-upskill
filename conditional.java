686e113e7181afaa14b20da4b7c9d007b43907f0
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-01-02 21:08:13 +0530

    if,else if,else

---
+++ conditional.java
@@ -0,0 +1,19 @@
+public class conditional {
+    public static void main(String[] args) {
+        char attendence='o';
+        if(attendence=='p')
+        {
+            System.out.println("attended class");
+        }
+        else if(attendence=='o')
+        {
+            System.out.println("oneduty");
+
+        }
+        else{
+            System.out.println("not attended class");
+        }
+        System.out.println("program ends");
+    }
+    
+}
