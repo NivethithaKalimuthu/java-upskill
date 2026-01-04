1a45d58947138aefd413838480dfcd2551af036a
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-01-04 14:32:56 +0530

    array

---
+++ array.java
@@ -0,0 +1,11 @@
+public class array {
+    public static void main(String[] args) {
+        int[] rollno=new int[5];
+        rollno[0]=5;
+        rollno[4]=6;
+        for(int i=0;i<5;i++){
+            System.out.println(rollno[i]);
+        }
+    }
+    
+}
