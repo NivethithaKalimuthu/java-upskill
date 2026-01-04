7f2ac5d17ee19a798652db2f817ba865a288a3fb
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-01-04 13:51:51 +0530

    iterative while

---
+++ dowhileloop.java
@@ -0,0 +1,14 @@
+public class dowhileloop {
+    public static void main(String[] args) {
+        int n=5;
+        int sum=0;
+        int i=1;
+        do{
+            sum+=i;
+            i++;
+        }while(i<=n);
+        System.out.println("sum:"+sum);
+    }
+
+    
+}
